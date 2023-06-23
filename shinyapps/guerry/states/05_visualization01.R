ba51e7ebf15b2d594511a00de1456e1ceb50be79
JsLth <jslth@outlook.com>
2023-06-23 22:16:39 +0200

    added raw code to visualization subsection

--- shinyapps/guerry/states/05_visualization01.R
+++ shinyapps/guerry/states/05_visualization01.R
@@ -1,25 +1,14 @@
 library(shiny)
 library(htmltools)
 library(bs4Dash)
-library(fresh)
-library(waiter)
 library(shinyWidgets)
 library(Guerry)
 library(sf)
 library(tidyr)
 library(dplyr)
-library(RColorBrewer)
-library(viridis)
-library(leaflet)
 library(plotly)
 library(jsonlite)
-library(ggplot2)
 library(GGally)
-library(datawizard)
-library(parameters)
-library(performance)
-library(ggdark)
-library(modelsummary)
 
 # Preparation ----
 
@@ -41,49 +30,6 @@ variables <- guerry %>%
   st_drop_geometry() %>%
   select(where(is.numeric) & !all_of(c("COUNT", "dept", "AVE_ID_GEO"))) %>%
   names()
-
-## Pivot ----
-guerry_long <- tidyr::pivot_longer(
-  st_drop_geometry(guerry),
-  cols = all_of(variables),
-  names_to = "variable"
-) %>%
-  mutate(variable = factor(variable, levels = unique(variable))) %>%
-  arrange(variable)
-
-## Aggregate ----
-guerry_region <- guerry %>%
-  group_by(Region) %>%
-  summarise(across(
-  .cols = all_of(variables),
-  function(x) {
-    if (cur_column() %in% c("Area", "Pop1831")) {
-      sum(x)
-    } else {
-      mean(x)
-    }
-  }
-))
-
-guerry_region_long <- tidyr::pivot_longer(
-  st_drop_geometry(guerry_region),
-  cols = all_of(variables),
-  names_to = "variable"
-) %>%
-  mutate(variable = factor(variable, levels = unique(variable))) %>%
-  arrange(variable)
-
-## Read text data ----
-txts <- read_json("../app_labels.json", simplifyVector = TRUE)
-
-## Prepare palettes ----
-pals <- list(
-  Sequential = RColorBrewer::brewer.pal.info %>%
-    filter(category %in% "seq") %>%
-    row.names(),
-  Viridis = c("Magma", "Inferno", "Plasma", "Viridis",
-              "Cividis", "Rocket", "Mako", "Turbo")
-)
 
 plotly_buttons <- c(
 	"sendDataToCloud", "zoom2d", "select2d", "lasso2d", "autoScale2d",
@@ -136,30 +82,12 @@ preloader <- list(
 
 ui <- dashboardPage(
   title = "The Guerry Dashboard",
-  freshTheme = dash_theme,
-  preloader = preloader,
   ## Header ----
   header = dashboardHeader(
-    tags$style("
-      /* remove white space from header */
-      .navbar {
-        padding-top: 0em;
-        padding-bottom: 0em;
-        padding-right: 0em;
-      }
-    "),
-    span(style = "display: inline-block; width: 100%;"),
-    a(
-      class = "logo",
-      href = "https://gesis.org/",
-      img(src = "../www/gesis-logo.png", style = "height: 1.8em;")
-    ),
     title = tagList(
       img(src = "../www/workshop-logo.png", width = 35, height = 35),
       span("The Guerry Dashboard", class = "brand-text")
-    ),
-    skin = "light",
-    sidebarIcon = tags$i(class = "fa fa-bars", style = "color: black;")
+    )
   ),
   ## Sidebar ----
   sidebar = dashboardSidebar(
@@ -178,10 +106,6 @@ ui <- dashboardPage(
   ),
   ## Body ----
   body = dashboardBody(
-    tags$head(
-      waiter::use_waiter(),
-      includeCSS("../www/styles.css")
-    ),
     tabItems(
     	tabItem("intro"),
     	tabItem("insp"),
