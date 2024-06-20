8c1c7e70a084c2e72a44b671efd87a4e9652f60f
JsLth <jslth@outlook.com>
2024-06-20 17:22:02 +0200

    adjust app to new data

--- shinyapps/app.R
+++ shinyapps/app.R
@@ -1,9 +1,11 @@
+library(dplyr)
+library(tidyr)
 library(shiny)
 library(plotly)
 library(leaflet)
 
-ess <- readRDS("../data/ess_clean.rds")
-ess_geo <- readRDS("../data/ess_geo.rds")
+ess <- readRDS("../data/ess_trust.rds")
+ess_geo <- readRDS("../data/ess_trust_geo.rds")
 
 # UI ----
 ui <- fluidPage(
@@ -12,18 +14,31 @@ ui <- fluidPage(
   ## Sidebar ----
   sidebarLayout(
     sidebarPanel(
+      ### select dependent variable
+      selectInput(
+        "xvar",
+        label = "Select a dependent variable",
+        choices = c(
+          "Trust in country's parliament" = "trust_parliament",
+          "Trust in the legal system" = "trust_legal",
+          "Trust in the police" = "trust_police",
+          "Trust in politicians" = "trust_politicians",
+          "Trust in political parties" = "trust_parties",
+          "Trust in the European Parliament" = "trust_eu",
+          "Trust in the United Nations" = "trust_un"
+        )
+      ),
       
       ### select a variable ----
       selectInput(
-        "variable",
-        label = "Select a variable",
+        "yvar",
+        label = "Select an independent variable",
         choices = c(
-          "Time spent consuming political news" = "nwspol",
-          "How often do you use the internet?" = "netusoft",
-          "Daily time spent on the internet" = "netustm",
-          "Most people can be trusted" = "ppltrst",
-          "Most people try to be fair" = "pplfair",
-          "Most people try to be helpful" = "pplhlp"
+          "Placement on the left-right scale" = "left_right",
+          "Age" = "age",
+          "Feeling about household's income" = "income_feeling",
+          "How often do you use the internet?" = "internet_use",
+          "How happy are you?" = "happiness"
         )
       ),
       
@@ -31,7 +46,7 @@ ui <- fluidPage(
       selectizeInput(
         "countries",
         label = "Filter by country",
-        choices = unique(ess$cntry),
+        choices = unique(ess$country),
         selected = "FR",
         multiple = TRUE
       ),
@@ -40,9 +55,10 @@ ui <- fluidPage(
       sliderInput(
         "range",
         label = "Set a value range",
-        min = min(ess$nwspol, na.rm = TRUE),
-        max = max(ess$nwspol, na.rm = TRUE),
-        value = range(ess$nwspol, na.rm = TRUE)
+        min = min(ess$trust_parliament, na.rm = TRUE),
+        max = max(ess$trust_parliament, na.rm = TRUE),
+        value = range(ess$trust_parliament, na.rm = TRUE),
+        step = 1
       )
     ),
     
@@ -81,7 +97,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   # update slider ----
   observe({
-    var <- na.omit(ess[[input$variable]])
+    var <- na.omit(ess[[input$xvar]])
     is_ordered <- is.ordered(var)
     var <- as.numeric(var)
     updateSliderInput(
@@ -92,50 +108,68 @@ server <- function(input, output) {
       step = if (is_ordered) 1
     )
   }) %>%
-    bindEvent(input$variable)
+    bindEvent(input$xvar)
   
   # filter data ----
   filtered <- reactive({
-    var <- input$variable
+    xvar <- input$xvar
+    yvar <- input$yvar
     range <- input$range
     
+    # select country
+    if (!is.null(input$countries)) {
+      ess <- ess[ess$country %in% input$countries, ]
+    }
+    
     # select variable
-    plot_data <- ess[c("idno", "cntry", var)]
-    plot_data[[var]] <- as.numeric(plot_data[[var]])
+    plot_data <- ess[c("idno", "country", xvar, yvar)]
     
     # apply range
     plot_data <- plot_data[
-      plot_data[[var]] > range[1] &
-      plot_data[[var]] < range[2], 
+      plot_data[[xvar]] > range[1] &
+      plot_data[[xvar]] < range[2], 
     ]
     plot_data
   })
   
   # render table ----
   output$table <- renderTable({
-    ess[ess$cntry %in% input$countries, ]
+    ess[ess$country %in% input$countries, ]
   }, height = 400)
-  
+
   # render plot ----
   output$plot <- renderPlotly({
-    plot_data <- filtered()
-    plot_data[[input$variable]]
-    p <- ggplot(plot_data) +
-      geom_histogram(aes(x = .data[[input$variable]])) +
-      scale_y_continuous(expand = c(0, 0)) +
+    xvar <- input$xvar
+    yvar <- input$yvar
+    plot_data <- filtered() %>%
+      drop_na() %>%
+      mutate(across(is.numeric, .fns = as.ordered))
+
+    xtab <- table(plot_data[c(xvar, yvar)]) %>%
+      prop.table(margin = 2) %>%
+      as_tibble() %>%
+      mutate(across(is.character, .fns = ~as.ordered(as.numeric(.x)))) %>%
+      mutate(n)
+
+    p <- ggplot(xtab) +
+      aes(x = .data[[yvar]], y = .data[[xvar]]) +
+      geom_tile(aes(fill = n), color = "black") +
+      scale_y_discrete(expand = c(0, 0)) +
+      scale_x_discrete(expand = c(0, 0)) +
+      scale_fill_viridis_c() +
       theme_classic()
     plotly::ggplotly(p)
   })
   
   # render map ----
   output$map <- renderLeaflet({
-    var <- input$variable
-    plot_data <- ess_geo[c("cntry", var)]
+    var <- input$xvar
+    plot_data <- ess_geo[c("country", var)]
     
     # create labels with a bold title and a body
     labels <- sprintf(
       "<strong>%s</strong><br>%s",
-      plot_data$cntry,
+      plot_data$country,
       plot_data[[var]]
     )
     labels <- lapply(labels, HTML)
@@ -178,4 +212,4 @@ server <- function(input, output) {
   })
 }
 
-runApp(shinyApp(ui=ui, server = server))
+shinyApp(ui = ui, server = server)
