9214cb8e39cd63cea1b18d4b4f190c385c7c41f1
Paul C. Bauer <paulcbauer@users.noreply.github.com>
2024-07-22 14:54:01 +0200

    Add app file for datatable (this is also included in the solution in the script)

---
+++ shinyapps/example/app_table_datatable.R
@@ -0,0 +1,129 @@
+library(tidyverse)
+library(shiny)
+library(plotly)
+library(leaflet)
+library(haven)
+
+ess <- readRDS("ess_trust.rds")
+ess_geo <- readRDS("ess_trust_geo.rds")
+
+# UI ----
+ui <- fluidPage(
+  titlePanel("European Social Survey - round 10"),
+  
+  ## Sidebar ----
+  sidebarLayout(
+    sidebarPanel(
+      ### select dependent variable
+      selectInput(
+        "xvar",
+        label = "Select a dependent variable",
+        choices = c(
+          "Trust in country's parliament" = "trust_parliament",
+          "Trust in the legal system" = "trust_legal",
+          "Trust in the police" = "trust_police",
+          "Trust in politicians" = "trust_politicians",
+          "Trust in political parties" = "trust_parties",
+          "Trust in the European Parliament" = "trust_eu",
+          "Trust in the United Nations" = "trust_un"
+        )
+      ),
+      
+      ### select a variable ----
+      selectInput(
+        "yvar",
+        label = "Select an independent variable",
+        choices = c(
+          "Placement on the left-right scale" = "left_right",
+          "Age" = "age",
+          "Feeling about household's income" = "income_feeling",
+          "How often do you use the internet?" = "internet_use",
+          "How happy are you?" = "happiness"
+        )
+      ),
+      
+      ### select a country ----
+      selectizeInput(
+        "countries",
+        label = "Filter by country",
+        choices = unique(ess$country),
+        selected = "FR",
+        multiple = TRUE
+      ),
+      
+      ### filter values ----
+      sliderInput(
+        "range",
+        label = "Set a value range (dependent variable)",
+        min = min(ess$trust_parliament, na.rm = TRUE),
+        max = max(ess$trust_parliament, na.rm = TRUE),
+        value = range(ess$trust_parliament, na.rm = TRUE),
+        step = 1
+      )
+    ),
+    
+    ## Main panel ----
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        
+        ### Table tab ----
+        tabPanel(
+          title = "Table",
+          div(
+            style = "height: 600px; overflow-y: auto;",
+            dataTableOutput("table")
+          )
+        )
+      )
+    )
+  )
+)
+
+
+# Server ----
+server <- function(input, output, session) {
+  # update slider ----
+  observe({
+    var <- na.omit(ess[[input$xvar]])
+    is_ordered <- is.ordered(var)
+    var <- as.numeric(var)
+    updateSliderInput(
+      inputId = "range",
+      min = min(var),
+      max = max(var),
+      value = range(var),
+      step = if (is_ordered) 1
+    )
+  }) %>%
+    bindEvent(input$xvar)
+  
+  # filter data ----
+  filtered <- reactive({
+    req(input$countries, cancelOutput = TRUE)
+    
+    xvar <- input$xvar
+    yvar <- input$yvar
+    range <- input$range
+    
+    # select country
+    ess <- ess[ess$country %in% input$countries, ]
+    
+    # select variable
+    ess <- ess[c("idno", "country", xvar, yvar)]
+    
+    # apply range
+    ess <- ess[ess[[xvar]] > range[1] & ess[[xvar]] < range[2], ]
+    ess
+  })
+  
+  # render table ----
+  output$table <- renderDataTable(filtered() %>% zap_labels(), 
+                                  options = list(pageLength = 5,
+                                                 searching = FALSE,
+                                                 paging = FALSE,
+                                                 ordering = FALSE,
+                                                 filtering = FALSE))
+}
+
+shinyApp(ui = ui, server = server)
